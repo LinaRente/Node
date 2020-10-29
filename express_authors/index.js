4e9c1b685329b153819cf7c6e05abc259cce21f3
LinaRente <linarente@gmail.com>
2020-10-29 08:54:28 +0100

    exo_0_1_2_3

--- express_authors/index.js
+++ express_authors/index.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 const authors = ['Lawrence Nowell, UK', 'William Shakespeare, UK', 'Charles Dickens, US','Oscar Wilde, UK' ]
 
+const books = ['Beowulf', 'Hamlet, Othello, Romeo and Juliet, MacBeth', 'Oliver Twist, A Christmas Carol', 'The Picture of Dorian Gray, The Importance of Being Earnest']
+
 // Exercices 0
 app.listen(port, () => {
     console.log('Authors Api')
@@ -15,3 +17,15 @@ app.get('/authors/:id', (req, res) => {
 });
 
 // Exercice 2
+app.get('/authors/:id/books/', (req, res) => {
+    res.send(books[req.params.id-1]);
+});
+
+// Exercice 3
+
+//error 404 foncionne
+app.get('*', (req, res) => {
+    res.send('Error 404');
+});
+
+// Execices 4
