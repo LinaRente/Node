08e0cd6cd431044fe2bf02022b28f3f442e3f06a
LinaRente <linarente@gmail.com>
2020-11-03 16:54:42 +0100

    end_simple_form

--- express_simple_form/index.js
+++ express_simple_form/index.js
@@ -1,8 +1,37 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
+
 const app = express();
+
 const port = 3000;
 
+const students = ['Jean', 'Binta', 'Agathe', 'Adil']
 
-app.listen(port, () => { 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: false,
+    layoutsDir: __dirname + "views/"
+}));
+
+
+app.get('/home/', function (req, res) {
+    res.render('home', {
+        title: 'Welcome to express simple form',
+        students: students,
+    });
+});
+
+app.post('/students/add', (req, res) => {
+    let studentsList = req.body.username;
+    students.push(studentsList)
+    res.render('list', {
+        studentsAdded: studentsList
+    })
+});
+
+app.listen(port, () => {
     console.log('Server started on port ' + port)
-})
+});
