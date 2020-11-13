ea57ecebe858446ec25a64aa7d033dd72efa559c
LinaRente <linarente@gmail.com>
2020-11-13 16:50:03 +0100

    etape4

--- express_upload/index.js
+++ express_upload/index.js
@@ -25,8 +25,6 @@ app.get('/', (req, res) => {
 //     console.log('mon form', req.body)
 // });
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -37,15 +35,13 @@ app.use(express.json());
 app.post("/upload", upload.single('image'), (req, res, next) => {
     let userprofil = req.body.username;
     userarray.push(userprofil)
-    let img = req.file.img
+    let img = req.file.path
     console.log(img)
     res.render("donnes", {
         username: userprofil,
         image : img
     });
 });
-
-
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -62,7 +58,7 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model('Car', CarSchema);
+const User = mongoose.model('User', UserSchema);
 
 User.create({
     username: 'IcePrincess',
@@ -75,3 +71,60 @@ User.create({
 app.listen(port, () => {
     console.log('Server started on port: ' + port);
 });
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = 3001;
+// const mongoose = require('mongoose');
+// const exphbs = require('express-handlebars');
+// const multer = require('multer');
+// const upload = multer({ dest: 'public/uploads/' });
+// //const path = require('path')
+// app.engine('handlebars', exphbs());
+// app.set('view engine', 'handlebars');
+// app.use(express.json());
+// app.use(express.static('public'));
+// mongoose.connect('mongodb://localhost:27017/upload',
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }
+// ).catch(error => console.log(error));
+// /// pour s'assuerer que le server est bien connecter 
+// mongoose.connection.on('connected', function () {
+//     console.log(`Database connection open to ${mongoose.connection.host} ${mongoose.connection.name}`);
+// });
+// mongoose.connection.on('error', function (err) {
+//     console.log('Mongoose default connection error: ' + err);
+// });
+// mongoose.connection.on('disconnected', function () {
+//     console.log('Mongoose default connection disconnected');
+// });
+// /////
+// const UserShema = new mongoose.Schema({
+//     username: { type: [String], index: true },
+//     firstName: String,
+//     surname: String,
+//     profilePicture: String
+// });
+// const UserModel = mongoose.model('User', UserShema);
+//  app.post('/upload', upload.single('image'), (req, res, next) => {   
+//     res.json(req.file);
+    
+//     const username= req.body.UserName;
+//     const image = req.file.path;
+//     console.log(req.body.Username);
+//    console.log(req.file.path)
+//    UserModel.create({
+//     username: username,
+//     profilePicture: image,
+// })
+// });
+// app.get('/', function (req, res) {
+//     res.render('home');
+//     //res.send('Welcome to express upload');
+// });
+// app.listen(PORT, () => {
+//     console.log(`Vous etes sur le prt ${PORT}`)
+// })
